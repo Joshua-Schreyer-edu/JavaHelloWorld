d63b736ef407b21efce1039550cf18b6cf1fcd79
Joshua-Schreyer <95540035+Joshua-Schreyer@users.noreply.github.com>
2025-08-31 17:59:48 +0000

    Add input capability

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,11 +7,18 @@
  * @since 2024-08-18
  */
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
     // this is where every Java program starts
   public static void main(String[] args) {
-    // Prints "Hello, World!" to the console
     System.out.println("Hello, World!");
+
+    Scanner sc = new Scanner(System.in);
+    String name = sc.next();
+    sc.close();
+
+    System.out.println("Hello, " + name + "!");
   }
 }
